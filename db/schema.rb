8ca7c412b0c376fd0241d29a087dc4e755af0756
Regina Xu <regina.xu1@gmail.com>
2015-03-05 21:40:22 -0800

    Models!

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150306030443) do
+ActiveRecord::Schema.define(version: 20150306053628) do
 
   create_table "cats", force: true do |t|
     t.string "name"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 20150306030443) do
     t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "age"
   end
 
 end
